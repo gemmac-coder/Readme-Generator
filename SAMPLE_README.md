a1dde14c4027a831f7d4c7d8171dfea4854c14a1
gemmacollinge <gemmacollinge_71@hotmail.com>
2021-05-03 19:23:19 +0100

    user questions array

---
+++ SAMPLE_README.md
@@ -0,0 +1,62 @@
+# Project Title
+
+(Question 1 -input)
+
+![mit license](https://img.shields.io/badge/license-MIT-green) (Question 2 i.e. choices)
+
+## Description of Project
+
+example description of projects here (Question 3 - input)
+
+## Table of Contents
+
+- [Project Title](#project-title)
+  - [Description of Project](#description-of-project)
+  - [Table of Contents](#table-of-contents)
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [License](#license)
+  - [Contributing](#contributing)
+  - [Tests](#tests)
+  - [Questions](#questions)
+
+## Installation
+
+```
+npm install
+node index.js
+```
+
+(Question 4 - ???)
+
+## Usage
+
+- When the app is running, answer all of the questions. After all questions, view generated README markdown file.
+
+(Question 5 - input?)
+
+## License
+
+MIT License
+
+(Will be same choice as Q2, don't ask question again as it will be same answer)
+
+## Contributing
+
+Open an issue or a pull request and I will take a look.
+
+(Question 6 - input)
+
+## Tests
+
+There are no tests for this application.
+
+(Question 7 - input)
+
+## Questions
+
+- View my [GitHub]() profile (Question 8 - input)
+
+- Email me at example@email.com
+
+(Question 9 - input)
--- index.js
+++ index.js
@@ -1,7 +1,60 @@
 // TODO: Include packages needed for this application
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+  {
+    type: "input",
+    message: "What is your application title?",
+    title: "applicationTitle",
+  },
+  {
+    // question 2 - license - list
+  },
+  {
+    type: "input",
+    message: "How would you describe what your application is about?",
+    description: "applicationDescription",
+  },
+  {
+    // table of contents
+  },
+  {
+    type: "input",
+    message:
+      "What installation instructions would you give for your application?",
+    description: "installationInstructions",
+  },
+  {
+    type: "input",
+    message: "What usage instructions would you give for your application?",
+    description: "usageInstructions",
+  },
+  {
+    // license
+  },
+  {
+    type: "input",
+    message:
+      "What contribution instructions can you give for your application?",
+    description: "contributionInstructions",
+  },
+  {
+    type: "input",
+    message: "Can you give any information on tests for your application?",
+    description: "testInformation",
+  },
+  {
+    type: "input",
+    message: "Can you enter your GitHub profile URL?",
+    description: "githubProfile",
+  },
+  {
+    type: "input",
+    message:
+      "Can you enter your email address for anyone wishing to get in touch?",
+    description: "emailAddress",
+  },
+];
 
 // TODO: Create a function to write README file
 const writeToFile = (fileName, data) => {};
